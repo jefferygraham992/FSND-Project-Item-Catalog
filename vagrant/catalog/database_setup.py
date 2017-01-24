ba8a035a0624f4562d38ea2bc702e2d2e83c8580
Jeffery Graham <jefferygraham496@gmail.com>
2017-01-24 13:09:59 -0500

    create database for project

---
+++ vagrant/catalog/database_setup.py
@@ -0,0 +1,72 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'id': self.id,
+            'picture': self.picture
+        }
+
+
+class Category(Base):
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'id': self.id
+        }
+
+
+class LegoSet(Base):
+    __tablename__ = 'lego_set'
+
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    pieces = Column(Integer)
+    lot_id = Column(Integer)
+    description = Column(String(250))
+    picture = Column(String(250))
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'description': self.description,
+            'id': self.id,
+            'pieces': self.pieces,
+            'lot_id': self.lot_id,
+            'picture': self.picture
+        }
+
+
+engine = create_engine('sqlite:///catalogitemswithusers.db')
+
+
+Base.metadata.create_all(engine)
