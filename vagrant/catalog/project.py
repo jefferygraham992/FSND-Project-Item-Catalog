b60174090e9dbd6c5893b9d3f553f4858c2a7abe
Jeffery Graham <jefferygraham496@gmail.com>
2017-03-07 14:47:50 -0500

    update project.py for PEP8 compliance

--- vagrant/catalog/project.py
+++ vagrant/catalog/project.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, redirect, jsonify, url_for, flash
+from flask import Flask, render_template, request, redirect, jsonify, url_for
+from flask import flash
 from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, CharacterType, Character, User
@@ -14,7 +15,8 @@ import requests
 
 app = Flask(__name__)
 
-CLIENT_ID = json.loads(open('client_secrets.json', 'r').read())['web']['client_id']
+CLIENT_ID = json.loads(open('client_secrets.json', 'r').read())['web']
+['client_id']
 
 # Connect to Database and create database session
 engine = create_engine('sqlite:///thomascatalog.db')
@@ -47,8 +49,7 @@ def fbconnect():
         'web']['app_id']
     app_secret = json.loads(
         open('fb_client_secrets.json', 'r').read())['web']['app_secret']
-    url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' % (
-        app_id, app_secret, access_token)
+    url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' % (app_id, app_secret, access_token)  # noqa
     h = httplib2.Http()
     result = h.request(url, 'GET')[1]
 
@@ -56,7 +57,6 @@ def fbconnect():
     userinfo_url = "https://graph.facebook.com/v2.4/me"
     # strip expire tag from access token
     token = result.split("&")[0]
-
 
     url = 'https://graph.facebook.com/v2.4/me?%s&fields=name,id,email' % token
     h = httplib2.Http()
@@ -69,12 +69,11 @@ def fbconnect():
     login_session['email'] = data["email"]
     login_session['facebook_id'] = data["id"]
 
-    # The token must be stored in the login_session in order to properly logout, let's strip out the information before the equals sign in our token
     stored_token = token.split("=")[1]
     login_session['access_token'] = stored_token
 
     # Get user picture
-    url = 'https://graph.facebook.com/v2.4/me/picture?%s&redirect=0&height=200&width=200' % token
+    url = 'https://graph.facebook.com/v2.4/me/picture?%s&redirect=0&height=200&width=200' % token  # noqa
     h = httplib2.Http()
     result = h.request(url, 'GET')[1]
     data = json.loads(result)
@@ -94,7 +93,7 @@ def fbconnect():
     output += '!</h1>'
     output += '<img src="'
     output += login_session['picture']
-    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;">'
 
     flash("Now logged in as %s" % login_session['username'])
     return output
@@ -105,7 +104,7 @@ def fbdisconnect():
     facebook_id = login_session['facebook_id']
     # The access token must me included to successfully logout
     access_token = login_session['access_token']
-    url = 'https://graph.facebook.com/%s/permissions?access_token=%s' % (facebook_id,access_token)
+    url = 'https://graph.facebook.com/%s/permissions?access_token=%s' % (facebook_id, access_token)  # noqa
     h = httplib2.Http()
     result = h.request(url, 'DELETE')[1]
     return "you have been logged out"
@@ -163,7 +162,7 @@ def gconnect():
     stored_credentials = login_session.get('credentials')
     stored_gplus_id = login_session.get('gplus_id')
     if stored_credentials is not None and gplus_id == stored_gplus_id:
-        response = make_response(json.dumps('Current user is already connected.'),
+        response = make_response(json.dumps('Current user already connected.'),
                                  200)
         response.headers['Content-Type'] = 'application/json'
         return response
@@ -197,7 +196,7 @@ def gconnect():
     output += '!</h1>'
     output += '<img src="'
     output += login_session['picture']
-    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;">'
     flash("you are now logged in as %s" % login_session['username'])
     print "done!"
     return output
@@ -254,13 +253,13 @@ def gdisconnect():
 @app.route('/catalog/JSON')
 def catalogJSON():
     inventory = session.query(CharacterType).all()
-    return jsonify(inventory=[train_kind.serialize for train_kind in inventory])
+    return jsonify(catalog=[train_kind.serialize for train_kind in inventory])
 
 
 @app.route('/catalog/<train_type>/JSON')
 def trainsJSON(train_type):
-    trains = session.query(Character).filter_by(character_kind=train_type).all()
-    return jsonify(engines=[train.serialize for train in trains])
+    items = session.query(Character).filter_by(character_kind=train_type).all()
+    return jsonify(engines=[item.serialize for item in items])
 
 
 @app.route('/catalog/<train_type>/<train_name>/JSON')
@@ -276,9 +275,13 @@ def showCatalog():
     inventory = session.query(CharacterType).order_by(asc(CharacterType.id))
     latest_trains = session.query(Character).limit(10)
     if 'username' not in login_session:
-        return render_template('publicshowcatalog.html', inventory=inventory, latest_trains=latest_trains)
+        return render_template('publicshowcatalog.html',
+                               inventory=inventory,
+                               latest_trains=latest_trains)
     else:
-        return render_template('showcatalog.html', inventory=inventory, latest_trains=latest_trains)
+        return render_template('showcatalog.html',
+                               inventory=inventory,
+                               latest_trains=latest_trains)
 
 
 # show all trains in one category
@@ -286,12 +289,26 @@ def showCatalog():
 @app.route('/catalog/<train_type>/trains')
 def showTrains(train_type):
     inventory = session.query(CharacterType).all()
-    trainType = session.query(CharacterType).filter_by(type_name=train_type).one()
-    trains = session.query(Character).filter_by(character_kind=train_type).all()
+    trainType = (
+                 session.query(CharacterType)
+                 .filter_by(type_name=train_type)
+                 .one()
+                 )
+    trains = (
+              session.query(Character)
+              .filter_by(character_kind=train_type)
+              .all()
+              )
     if 'username' not in login_session:
-        return render_template('publicshowtrains.html', trainType=trainType, trains=trains, inventory=inventory)
+        return render_template('publicshowtrains.html',
+                               trainType=trainType,
+                               trains=trains,
+                               inventory=inventory)
     else:
-        return render_template('showtrains.html', trainType=trainType, trains=trains, inventory=inventory)
+        return render_template('showtrains.html',
+                               trainType=trainType,
+                               trains=trains,
+                               inventory=inventory)
 
 
 # show info for one train
@@ -299,10 +316,17 @@ def showTrains(train_type):
 def showTrain(train_type, train_name):
     train = session.query(Character).filter_by(character_name=train_name).one()
     creator = getUserInfo(train.user_id)
-    if 'username' not in login_session or creator.id != login_session['user_id']:
-        return render_template('publicshowtrain.html', train=train, creator=creator)
+    if (
+        ('username' not in login_session) or
+        (creator.id != login_session['user_id'])
+    ):
+        return render_template('publicshowtrain.html',
+                               train=train,
+                               creator=creator)
     else:
-        return render_template('showtrain.html', train=train, creator=creator)
+        return render_template('showtrain.html',
+                               train=train,
+                               creator=creator)
 
 
 # add a train
@@ -312,7 +336,12 @@ def addTrain():
         return redirect('/login')
     inventory = session.query(CharacterType).all()
     if request.method == 'POST':
-        newTrain = Character(character_name=request.form['character_name'], description=request.form['description'], character_kind=request.form['character_kind'], character_picture=request.form['character_picture'], user_id=login_session['user_id'])
+        newTrain = Character(character_name=request.form['character_name'],
+                             description=request.form['description'],
+                             character_kind=request.form['character_kind'],
+                             character_picture=request.form
+                             ['character_picture'],
+                             user_id=login_session['user_id'])
         session.add(newTrain)
         flash('%s successfully added!' % newTrain.character_name)
         session.commit()
@@ -327,9 +356,15 @@ def editTrain(train_name):
     if 'username' not in login_session:
         return redirect('/login')
     inventory = session.query(CharacterType).all()
-    editedTrain = session.query(Character).filter_by(character_name=train_name).one()
+    editedTrain = (
+                   session.query(Character)
+                   .filter_by(character_name=train_name)
+                   .one()
+                   )
     if editedTrain.user_id != login_session['user_id']:
-        return "<script>function myFunction() {alert('You are not authorized to edit this train. Please create your own train in order to edit.');}</script><body onload='myFunction()''>"
+        return "<script>function myFunction() {alert('You are not authorized"
+        " to edit this train. Please create your own train "
+        "in order to edit.');}</script><body onload='myFunction()''>"
     if request.method == 'POST':
         if request.form['character_name']:
             editedTrain.character_name = request.form['character_name']
@@ -344,7 +379,9 @@ def editTrain(train_name):
         session.commit()
         return redirect(url_for('showCatalog'))
     else:
-        return render_template('edittrain.html', editedTrain=editedTrain, inventory=inventory)
+        return render_template('edittrain.html',
+                               editedTrain=editedTrain,
+                               inventory=inventory)
 
 
 # delete a train
@@ -352,9 +389,15 @@ def editTrain(train_name):
 def deleteTrain(train_name):
     if 'username' not in login_session:
         return redirect('/login')
-    trainToDelete = session.query(Character).filter_by(character_name=train_name).one()
+    trainToDelete = (
+                     session.query(Character)
+                     .filter_by(character_name=train_name)
+                     .one()
+                     )
     if trainToDelete.user_id != login_session['user_id']:
-        return "<script>function myFunction() {alert('You are not authorized to delete this train. Please create your own train in order to delete.');}</script><body onload='myFunction()''>"
+        return "<script>function myFunction() {alert('You are not authorized "
+        "to delete this train. Please create your own train in order "
+        "to delete.');}</script><body onload='myFunction()''>"
     if request.method == 'POST':
         session.delete(trainToDelete)
         flash('%s successfuly deleted!' % trainToDelete.character_name)
